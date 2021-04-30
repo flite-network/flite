b4797d3c989750bb15d7882fb08584b57e14be2e
BuildTools <ilanluciano1@gmail.com>
2021-04-30 19:08:32 -0400

    Start creating navbar

--- client/components/Navbar.tsx
+++ client/components/Navbar.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import Link from 'next/link';
+import styles from './Navbar.module.scss'
 
 export default class Navbar extends React.Component {
     render() {
-      return <header>
-hihi
+      return <header className={styles.header}>
+            <h1 className={styles.title}>FLITE</h1>
       </header>;
     }
 }
