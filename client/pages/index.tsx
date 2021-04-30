321ffbf830bbee12a4664f64c071a092d0a563ca
BuildTools <ilanluciano1@gmail.com>
2021-04-30 18:35:15 -0400

    Attempt to add navbar

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import Navbar from '../components/Navbar';
 
 class Home extends React.Component {
     render() {
         return(
-            <p>Hello World!</p>
+            <Navbar />
         );
     }
 }
