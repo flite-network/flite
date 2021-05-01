a345b2507350d24c6cd92f1cf4ab282463197072
BuildTools <ilanluciano1@gmail.com>
2021-05-01 11:53:01 -0400

    Create button component

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -2,13 +2,19 @@ import useSwr from 'swr';
 
 import React from 'react';
 import Navbar from '../components/Navbar';
+import Button from '../components/Button';
 
 const Index = () => {
     const {data, error} = useSwr('api/test');
     console.log(data);
 
     return (
-         <Navbar />
+        <>
+            <Navbar />
+            <div className="login-body">
+                <Button name="Login" />
+            </div>
+        </>
     );
 }
 
